e2b3bae7850f041fca4ffa966dc9830f9194d14a
Ger Mecias <carlosgermanmecias2002@gmail.com>
2025-09-09 15:06:19 -0300

    Funciones directorio agregadas

--- Laboratorio1_tp5/src/laboratorio1_tp5/Directorio_Telefonico.java
+++ Laboratorio1_tp5/src/laboratorio1_tp5/Directorio_Telefonico.java
@@ -1,13 +1,17 @@
 package laboratorio1_tp5;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class Directorio_Telefonico {
 
-    private TreeMap<Long, Contacto> n;
+    private TreeMap<Long, Contacto> listaContactos;
 
     public Directorio_Telefonico() {
-        this.n = new TreeMap<>();
+        this.listaContactos = new TreeMap<>();
     }
     Contacto c1 = new Contacto(12345678, "West", "Kayne", "San Luis", "Av. Siempre Viva 123");
     Contacto c2 = new Contacto(87654321, "Messi", "Leonel", "Rosario", "Los alpes");
@@ -15,27 +19,52 @@ public class Directorio_Telefonico {
     Contacto c4 = new Contacto(981254, "Wayne", "bruce", "Gotham", "Baticueva");
 
     public void agregarContacto(Long telefono, Contacto contacto) {
-        if (n.containsKey(telefono)) {
+        if (listaContactos.containsKey(telefono)) {
             System.out.println("Ya existe ese telefono");
             return;
         }
-        n.put(telefono, contacto);
+        listaContactos.put(telefono, contacto);
 
     }
 
-    public void buscarContacto() {
-
+    public Contacto buscarContacto(Long telefono) {
+        Contacto contacto;
+        if (listaContactos.containsKey(telefono)) {
+            contacto=listaContactos.get(telefono);
+            return contacto;
+        }else{
+            System.out.println("El numero de telefono no se encuentra asociado a ningun numero");
+        }
+        return null;  
     }
 
-    public void buscarTelefono() {
-
+    public Set<Long> buscarTelefono(String apellido) {
+        Set<Long> numeros=new HashSet<>();
+        for(Map.Entry<Long,Contacto> entry : listaContactos.entrySet()){
+            Contacto contacto= entry.getValue();
+            if(contacto.getApellido().equalsIgnoreCase(apellido)){
+                numeros.add(entry.getKey());
+            }
+        }
+        return numeros;
     }
 
-    public void buscarContactos() {
-
+    public ArrayList<Contacto> buscarContactos(String ciudad) {
+        ArrayList<Contacto> contactos=new ArrayList<>();
+        for(Map.Entry<Long,Contacto> entry : listaContactos.entrySet()){
+            Contacto contacto= entry.getValue();
+            if(contacto.getCiudad().equalsIgnoreCase(ciudad)){
+                contactos.add(contacto);
+            }
+        }
+        return contactos;
     }
 
-    public void borrarContacto() {
-
+    public void borrarContacto(Long telefono) {
+         if (listaContactos.containsKey(telefono)) {
+            listaContactos.remove(telefono);
+        }else{
+            System.out.println("El numero de telefono no se encuentra asociado a ningun numero");
+        }
     }
 }
